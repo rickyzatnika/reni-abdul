a50f98b8398e0903abacfdd4cb78417de49f7ee2
rickyzatnika <rickyzatnika91@gmail.com>
2023-05-12 17:47:50 +0700

    test2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 
 import { BrowserRouter as Router, } from "react-router-dom";
 import AnimatedRoutes from "./components/AnimateRoutes";
-import { ParallaxProvider } from 'react-scroll-parallax';
+
 function App() {
 
   return (
     <>
-      <ParallaxProvider>
 
-        <Router>
-          <AnimatedRoutes />
-        </Router>
-      </ParallaxProvider>
+      <Router>
+        <AnimatedRoutes />
+      </Router>
+
 
 
 
