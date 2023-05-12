7baf01dde883c7954ccd8f7c1dca94477e18fec0
rickyzatnika <rickyzatnika91@gmail.com>
2023-05-12 18:07:08 +0700

    test2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 
 const root = ReactDOM.createRoot(document.getElementById('_yups'));
-root.render(<App />);
+root.render(
+    <ParallaxProvider>
+        <App />
+    </ParallaxProvider>
+);
 
